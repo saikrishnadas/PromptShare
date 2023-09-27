6b2c0d2ed639e4908abe33daa6669c1573b9aa27
Sai <sai@SAIs-MacBook-Pro.local>
2023-09-27 16:28:16 +0530

    adding nav bar

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import Nav from "@components/Nav";
+import "@styles/globals.css";
+
+export const metadata = {
+    title:"PromptShare",
+    description:"iasdfhkjasndniasbndj"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient"/>
+            </div>
+
+            <main className="app">
+                <Nav />
+                {children}
+            </main>
+        </body>
+    </html>
+
+    )
+}
+
+export default RootLayout
